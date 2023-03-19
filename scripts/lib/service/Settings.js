7824bb2dabe619bc1ec33c816a744ee643712a82
zhangkunyuan <zhangkunyuan@cmhi.chinamobile.com>
2023-03-19 13:41:35 +0800

    修复BUG

--- scripts/lib/service/Settings.js
+++ scripts/lib/service/Settings.js
@@ -8,6 +8,16 @@ function getFileManager() {
   return fm;
 }
 
+function getSettingsDir() {
+  const fm = getFileManager();
+  const script_dir = fm.documentsDirectory()
+  const settings_dir = fm.joinPath(script_dir, "settings");
+  if (!fm.fileExists(settings_dir)) {
+    fm.createDirectory(settings_dir, true);
+  }
+  return settings_dir;
+}
+
 function saveDataToSettings(project_name, key, value) {
   let settings = loadSettings(project_name);
   if (settings == null) {
@@ -27,28 +37,14 @@ function getDataFromSettings(project_name, key) {
 
 function writeSettings(project_name, settings) {
   const fm = getFileManager();
-  const script_dir = module.filename.replace(
-    fm.fileName(module.filename, true),
-    ""
-  );
-  const settings_dir = fm.joinPath(script_dir, "settings");
-  if (!fm.fileExists(settings_dir)) {
-    fm.createDirectory(settings_dir, true);
-  }
+  const settings_dir = getSettingsDir();
   const settings_file = fm.joinPath(settings_dir, `${project_name}.json`);
   fm.writeString(settings_file, JSON.stringify(settings));
 }
 
 function loadSettings(project_name) {
   const fm = getFileManager();
-  const script_dir = module.filename.replace(
-    fm.fileName(module.filename, true),
-    ""
-  );
-  const settings_dir = fm.joinPath(script_dir, "settings");
-  if (!fm.fileExists(settings_dir)) {
-    fm.createDirectory(settings_dir, true);
-  }
+  const settings_dir = getSettingsDir();
   const settings_file = fm.joinPath(settings_dir, `${project_name}.json`);
   if (fm.fileExists(settings_file)) {
     try {
