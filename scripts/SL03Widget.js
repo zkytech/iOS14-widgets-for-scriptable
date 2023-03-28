02904e713abc75cebe6eee51fb5d0b181156d803
zhangkunyuan <zhangkunyuan@cmhi.chinamobile.com>
2023-03-28 19:50:18 +0800

    修正branch

--- scripts/SL03Widget.js
+++ scripts/SL03Widget.js
@@ -7,7 +7,7 @@
  * 联系邮箱: zhangkunyuan@hotmail.com
  *
  *
- * 参数获取和填写方法见文档: https://public.zkytech.top/iOS14-widgets-for-scriptable#4-%E6%B7%B1%E8%93%9Dsl03%E8%BD%A6%E8%BE%86%E7%8A%B6%E6%80%81
+ * 参数获取和填写方法见文档: https://public.zkytech.top/iOS14-widgets-for-scriptable/${branch}#4-%E6%B7%B1%E8%93%9Dsl03%E8%BD%A6%E8%BE%86%E7%8A%B6%E6%80%81
  * - 组件依赖深蓝APP登录信息（refresh_token）
  * - 本组件仅用于学习交流
  * - 本组件为开源软件，不会进行收费！！！
@@ -17,7 +17,7 @@
  */
 try {
   // 开发环境切换到dev分支，生产环境用master分支
-  const branch = "master";
+  const branch = "dev";
   const project_name = "深蓝小组件_by_zkytech";
   // const force_download = branch != "master";
   const force_download = true;
@@ -107,17 +107,17 @@ try {
     getChargeStatus,
   } = await getService(
     "SL03Api",
-    `https://public.zkytech.top/iOS14-widgets-for-scriptable/lib/service/SL03Api.js`,
+    `https://public.zkytech.top/iOS14-widgets-for-scriptable/${branch}/lib/service/SL03Api.js`,
     force_download
   );
   const { update } = await getService(
     "UpdateScript",
-    `https://public.zkytech.top/iOS14-widgets-for-scriptable/lib/service/UpdateScript.js`,
+    `https://public.zkytech.top/iOS14-widgets-for-scriptable/${branch}/lib/service/UpdateScript.js`,
     force_download
   );
   let { getDataFromSettings, saveDataToSettings } = await getService(
     "Settings",
-    `https://public.zkytech.top/iOS14-widgets-for-scriptable/lib/service/Settings.js`,
+    `https://public.zkytech.top/iOS14-widgets-for-scriptable/${branch}/lib/service/Settings.js`,
     force_download
   );
   function getSetting(key) {
@@ -130,7 +130,7 @@ try {
   if (branch == "master") {
     // 更新组件代码
     await update(
-      `https://public.zkytech.top/iOS14-widgets-for-scriptable/SL03Widget.js`
+      `https://public.zkytech.top/iOS14-widgets-for-scriptable/${branch}/SL03Widget.js`
     );
   }
 
@@ -162,7 +162,7 @@ try {
   async function renderAccessoryCircularWidget() {
     const { drawArc } = await getService(
       "DrawShape",
-      `https://public.zkytech.top/iOS14-widgets-for-scriptable/lib/service/DrawShape.js`,
+      `https://public.zkytech.top/iOS14-widgets-for-scriptable/${branch}/lib/service/DrawShape.js`,
       force_download
     );
     const params = args.widgetParameter
